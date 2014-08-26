b8f74c0d81e1db7ffea2f2420df11677374008eb
Babu K <babuk.1989@gmail.com>
2014-08-26 12:57:14 +0530

    chap3_progm3

---
+++ chap3_progm3.c
@@ -0,0 +1,17 @@
+/* Chapter 3:program 3*/
+
+/*Program in arithmetic*/
+
+/*Babu K DBS*/
+
+#include<stdio.h>
+int main(void)
+{
+	int answer,result;
+	
+	answer=100;
+	result=100-10;
+	printf("The result value is %i\n",result+5);
+	
+	return 0;
+}
