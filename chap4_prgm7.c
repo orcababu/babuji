7e5d45ae8c951fbcc80d7e36e1d70dcf066eb074
Babu K <babuk.1989@gmail.com>
2014-08-28 09:52:10 +0530

    chap4_prgm5

---
+++ chap4_prgm7.c
@@ -0,0 +1,23 @@
+/* Chapter 4:program 7*/
+
+/*Program in Excersice no : 5*/
+
+/*Babu K DBS*/
+
+#include<stdio.h>
+
+int main(void)
+
+{
+
+	char c,d;
+
+	c='d';
+
+	d=c;
+	
+	printf("d=%c\n",d);
+	
+	return 0;
+
+}
