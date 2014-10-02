c47bbe61087c0ffbf731efbe09a717562c336723
Babu K <babuk.1989@gmail.com>
2014-10-02 13:39:02 +0530

    chap2

---
+++ dromy/chap_2/ch2.3_sum.c
@@ -0,0 +1,28 @@
+//program 2.3
+
+//summation of set of n numbers
+
+//Babu  K DBS
+
+#include<stdio.h>
+
+int main(void)
+
+{
+	int i,j,n,sum=0;
+	printf("Enter the number of elements to sum\n");
+	scanf("%i",&n);
+	int a[n];
+	printf("Enter the %i numbers",n);
+	for(i=0;i<n;i++)
+		scanf("%i",&a[i]);
+		
+	for(i=0;i<n;i++)	
+	{
+		sum=sum+a[i];
+		
+	}
+	printf("The sum of entered numbers is %i",sum);
+	
+	return 0;
+}
